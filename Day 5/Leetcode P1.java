76e02753ea8be423c4d109ba429aa8bef1348b1c
jyotiraditya06 <adityapatil065@gmail.com>
2024-03-05 09:44:51 +0530

    Leetcode 1750. Minimum Length of String After Deleting Similar Ends

---
+++ Day 5/Leetcode P1.java
@@ -0,0 +1,17 @@
+// Leetcode 1750. Minimum Length of String After Deleting Similar Ends
+
+class Solution {
+    public int minimumLength(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right && s.charAt(left) == s.charAt(right)) {
+            char ch = s.charAt(left);
+            while (left <= right && s.charAt(left) == ch)
+                left++;
+            while (left <= right && s.charAt(right) == ch)
+                right--;
+        }
+
+        return Math.max(0, right - left + 1);
+    }
+}
