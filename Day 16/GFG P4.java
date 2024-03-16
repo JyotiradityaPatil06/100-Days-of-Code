279d470a8567c4e7cd6756238d1d57bf7742d66e
jyotiraditya06 <adityapatil065@gmail.com>
2024-03-16 16:14:48 +0530

    GFG: Median of 2 Sorted Arrays of Different Sizes

---
+++ Day 16/GFG P4.java
@@ -0,0 +1,73 @@
+// GFG: Median of 2 Sorted Arrays of Different Sizes
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Driver {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = sc.nextInt();
+
+            int m = sc.nextInt();
+            int[] b = new int[m];
+            for (int i = 0; i < m; i++)
+                b[i] = sc.nextInt();
+
+            double res = new GFG().medianOfArrays(n, m, a, b);
+
+            if (res == (int) res)
+                System.out.println((int) res);
+            else
+                System.out.println(res);
+        }
+
+    }
+}
+// } Driver Code Ends
+
+// User function Template for Java
+
+class GFG {
+    static double medianOfArrays(int n, int m, int a[], int b[]) {
+        int[] temp = new int[n + m];
+        int i = 0, j = 0, k = 0;
+
+        while (i < n && j < m) {
+            if (a[i] < b[j]) {
+                temp[k] = a[i];
+                i++;
+            } else {
+                temp[k] = b[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n) {
+            temp[k] = a[i];
+            i++;
+            k++;
+        }
+
+        while (j < m) {
+            temp[k] = b[j];
+            j++;
+            k++;
+        }
+
+        int mid = temp.length / 2;
+        if (temp.length % 2 != 0)
+            return (double) temp[mid];
+        else
+            return (double) (temp[mid - 1] + temp[mid]) / 2;
+    }
+}
