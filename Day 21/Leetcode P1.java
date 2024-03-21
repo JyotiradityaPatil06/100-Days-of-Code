03fd4d2946e7597fd10cea115460093550b8faa7
jyotiraditya06 <adityapatil065@gmail.com>
2024-03-21 10:08:13 +0530

    Leetcode 206. Reverse Linked List

---
+++ Day 21/Leetcode P1.java
@@ -0,0 +1,16 @@
+// Leetcode 206. Reverse Linked List
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
