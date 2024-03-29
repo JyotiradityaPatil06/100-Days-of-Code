3ec0c7ab542bfa36bc66c24f5e55d5c80130d126
jyotiraditya06 <adityapatil065@gmail.com>
2024-03-29 22:10:14 +0530

    Leetcode 2962. Count Subarrays Where Max Element Appears at Least K Times

---
+++ Day 29/Leetcode P1.java
@@ -0,0 +1,16 @@
+// Leetcode 2962. Count Subarrays Where Max Element Appears at Least K Times
+
+class Solution {
+    public long countSubarrays(int[] A, int k) {
+        int a = Arrays.stream(A).max().getAsInt();
+        int n = A.length, cur = 0, i = 0;
+        long res = 0;
+        for (int j = 0; j < n; ++j) {
+            cur += A[j] == a ? 1 : 0;
+            while (cur >= k)
+                cur -= A[i++] == a ? 1 : 0;
+            res += i;
+        }
+        return res;
+    }
+}
